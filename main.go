bb32da3cf31c6f40cceeac8dcc3fff0e5511919b
Olivier Ory <mail@normegil.be>
2017-03-13 09:44:13 +0100

    Logging added

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/sirupsen/logrus"
 )
+
+const PORT int = 8080
+
+func init() {
+
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Test")
 	})
-	http.ListenAndServe(":8080", nil)
+
+	logrus.WithField("port", PORT).Info("Launch server")
+	http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
 }
