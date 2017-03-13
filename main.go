8cc833ddaa89f1252bb3bf98efc97a4efe820cee
Olivier Ory <mail@normegil.be>
2017-03-13 10:57:30 +0100

    Logging: Add rotating log files

--- main.go
+++ main.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/Sirupsen/logrus"
+	logrotation "github.com/lestrrat/go-file-rotatelogs"
 	"github.com/rifflock/lfshook"
 )
 
+const LOG_PATH string = "/tmp/"
 const PORT int = 8080
 
 var log *logrus.Entry = logrus.NewEntry(logrus.New())
 
 func init() {
-	log = log.WithField("Test", "123")
-	log.Logger.Hooks.Add(lfshook.NewHook(lfshook.PathMap{
-		logrus.InfoLevel:  "info.log",
-		logrus.ErrorLevel: "error.log",
+	extention := "log"
+	log.Logger.Hooks.Add(lfshook.NewHook(lfshook.WriterMap{
+		logrus.InfoLevel:  NewLogRotation(LOG_PATH, "zookeeper-rest.info", extention),
+		logrus.ErrorLevel: NewLogRotation(LOG_PATH, "zookeeper-rest.error", extention),
 	}))
 }
 
@@ -28,4 +31,16 @@ func main() {
 
 	log.WithField("port", PORT).Info("Launch server")
 	http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
+}
+
+func NewLogRotation(path string, name string, extention string) *logrotation.RotateLogs {
+	pattern := "%Y-%m-%d"
+	separator := "."
+
+	day := time.Duration(24) * time.Hour
+	return logrotation.New(
+		path+name+separator+pattern+separator+extention,
+		logrotation.WithLinkName(path+name+separator+extention),
+		logrotation.WithMaxAge(time.Duration(7)*day),
+	)
 }
