44f6434b48672518312c77f272dca16f31397260
Olivier Ory <mail@normegil.be>
2017-03-14 17:04:20 +0100

    Implement update & Merge it with creation code

--- api/node/write.go
+++ api/node/write.go
@@ -1,71 +1,25 @@
 package node
 
 import (
-	"encoding/json"
 	"errors"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	apiErrors "github.com/normegil/zookeeper-rest/modules/errors"
 	"github.com/normegil/zookeeper-rest/modules/tools"
-	"github.com/samuel/go-zookeeper/zk"
 )
-
-type node struct {
-	Path    string
-	Data    string
-	Version int
-}
 
 func (c Controller) createOrUpdate(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	path := r.URL.Query().Get("path")
 	if "" == path {
-		c.create(w, r, p)
-		return
-	}
-	c.update(w, r, p)
-}
-
-func (c Controller) create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var toCreate node
-	err := json.Unmarshal(tools.ToBytes(r.Body), &toCreate)
-	if nil != err {
+		err := apiErrors.NewErrWithCode(40001, errors.New("Resource path not specified."))
 		c.handleError(w, err)
 		return
 	}
 
-	if "" == toCreate.Path {
-		err = apiErrors.NewErrWithCode(40001, errors.New("Resource path not specified."))
-		c.handleError(w, err)
-		return
-	}
-
-	acls := zk.WorldACL(zk.PermAll)
-	if err := c.Zookeeper().Create(toCreate.Path, []byte(toCreate.Data), acls); nil != err {
+	if err := c.Zookeeper().Set(path, tools.ToBytes(r.Body)); nil != err {
 		c.handleError(w, err)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-}
-
-func (c Controller) update(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var toUpdate node
-	err := json.Unmarshal(tools.ToBytes(r.Body), &toUpdate)
-	if nil != err {
-		c.handleError(w, err)
-		return
-	}
-
-	toUpdate.Path = r.URL.Query().Get("path")
-	if "" == toUpdate.Path {
-		err = apiErrors.NewErrWithCode(40001, errors.New("Resource path not specified."))
-		c.handleError(w, err)
-		return
-	}
-
-	err = c.Zookeeper().Set(toUpdate.Path, []byte(toUpdate.Data), toUpdate.Version)
-	if nil != err {
-		c.handleError(w, err)
-		return
-	}
 }
--- modules/zookeeper/set.go
+++ modules/zookeeper/set.go
@@ -7,7 +7,7 @@ import (
 	"github.com/samuel/go-zookeeper/zk"
 )
 
-func (z Zookeeper) Set(path string, content []byte, version int) error {
+func (z Zookeeper) Set(path string, content []byte) error {
 	connection, _, err := zk.Connect([]string{z.Address}, time.Second)
 	if nil != err {
 		return err
@@ -15,9 +15,24 @@ func (z Zookeeper) Set(path string, content []byte, version int) error {
 	defer connection.Close()
 	connection.SetLogger(log.VoidLogger{})
 
-	_, err = connection.Set(path, content, int32(version))
+	exist, _, err := connection.Exists(path)
 	if nil != err {
-		return  err
+		return err
+	}
+
+	if exist {
+		_, stat, err := connection.Get(path)
+		if nil != err {
+			return err
+		}
+		_, err = connection.Set(path, content, stat.Version)
+	} else {
+		var noFlag int32 = 0
+		_, err = connection.Create(path, content, noFlag, zk.WorldACL(zk.PermAll))
+	}
+
+	if nil != err {
+		return err
 	}
 	return nil
 }
